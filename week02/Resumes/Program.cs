eeb8ca085416fef988676fb7fd2d190202ed7e47
woodrow-snow <woodrow2021@outlook.com>
2025-09-08 10:27:12 -0600

    completed the week 2 learning activity program, updated program.cs and added job.cs and resume.cs

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        // creating and displaying job 1
+        Job job1 = new Job();
+        job1._company = "Microsoft";
+        job1._jobTitle = "Software Engineer";
+        job1._endYear = 2012;
+        job1._startYear = 2008;
+        job1.Display();
+
+        // creating and displaying job 2
+        Job job2 = new Job();
+        job2._company = "Apple";
+        job2._jobTitle = "Senior Software Engineer";
+        job2._endYear = 2025;
+        job2._startYear = 2012;
+        job2.Display();
+
+        // breaking line
+        Console.WriteLine();
+
+        // creating and new resume obj and adding jobs
+        Resume myResume = new Resume();
+        myResume._name = "Allison Rose";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        // displaying jobs again
+        myResume.Display();
+        
     }
 }
